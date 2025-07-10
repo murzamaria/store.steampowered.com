f1a3877f72efb1d8bc73f5a07323d3b33524b913
Maryia Murza <murzamaria156@gmail.com>
2025-07-10 23:12:57 +0300

    Task 9 adding CI

--- custom-logger.ts
+++ custom-logger.ts
@@ -1,12 +1,24 @@
 import type { Logger } from '@playwright/test';
+import fs from 'fs';
+import path from 'path';
 
 export class CustomLogger implements Logger {
+  private logFilePath: string;
+
+  constructor() {
+    this.logFilePath = path.join('logs', 'test.log');
+
+    fs.writeFileSync(this.logFilePath, 'Tests started\n', { flag: 'w' });
+  }
+
   isEnabled(name: string, severity: 'error' | 'warning' | 'info' | 'verbose'): boolean {
     return severity === 'error' || severity === 'warning';
   }
 
   log(name: string, severity: 'error' | 'warning' | 'info' | 'verbose', message: string): void {
     const time = new Date().toISOString();
-    console.log(`[${time}] [${name}] [${severity.toUpperCase()}]: ${message}`);
+    const formatted = `[${time}] [${name}] [${severity.toUpperCase()}]: ${message}`;
+    console.log(formatted);
+    fs.appendFileSync(this.logFilePath, formatted + '\n');
   }
 }
