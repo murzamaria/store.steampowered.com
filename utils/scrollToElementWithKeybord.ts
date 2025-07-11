f98be3dd614b5d7249beab62f65a32b4bcdfc5f7
Maryia Murza <murzamaria156@gmail.com>
2025-07-11 15:28:34 +0300

    Working on artifacts&test failing

---
+++ utils/scrollToElementWithKeybord.ts
@@ -0,0 +1,19 @@
+import { Locator, Page } from '@playwright/test';
+
+export async function scrollToElementWithKeyboard(
+  page: Page,
+  locator: Locator,
+  key: 'PageDown' | 'ArrowDown' = 'PageDown',
+  maxScrolls = 10,
+  timeout = 300,
+) {
+  for (let i = 0; i < maxScrolls; i++) {
+    const isVisible = await locator.isVisible().catch(() => false);
+    if (isVisible) return true;
+
+    await page.keyboard.press(key);
+    await page.waitForTimeout(timeout);
+  }
+
+  throw new Error('Element not found after scrolling');
+}
